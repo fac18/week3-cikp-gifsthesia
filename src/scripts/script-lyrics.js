8b5af625f1e3d131c3d9f87a91795a9702a48606
Kin Au <kinnamau@gmail.com>
2019-11-13 17:26:21 +0000

    started writing get tracklist function
    Co-authored-by:Reda <redahaq@gmail.com>
    relates #2

--- src/scripts/script-lyrics.js
+++ src/scripts/script-lyrics.js
@@ -1,4 +1,25 @@
-function x() {
+"use strict";
 
+// let tracksObj = {};
+// tracksObj.keys
+// response.message.body.tracklist
+
+function log(){
+    return "testytest";
 }
 
+function getTrackList() {
+    let xhr = new XMLHttpRequest();
+    let url = "https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=a515bb508047c4dbf594264ecfde2094&chart_name=top&page=1&page_size=50&country=uk&f_has_lyrics=1"
+    xhr.onreadystatechange = function() {
+        if (xhr.readystate === 4 && xhr.status === 200) {
+            let response = JSON.parse(xhr.responseText);
+            // console.log(response.message.body.track_list.track.track_name);
+            return response.message.body.track_list.track.track_name;
+        }
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
+}
+
+module.exports = log;
