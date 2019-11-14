4ec8f5d896c840151b88d67b9038129987a2fe79
Roshan Thaabith <roshlarosh@gmail.com>
2019-11-14 17:52:53 +0000

    made the gimme another hint button responsive Fixes #22

--- src/scripts/script-run.js
+++ src/scripts/script-run.js
@@ -6,6 +6,7 @@ newGameButton.addEventListener('click', function() {
     getRandomTrackInfo(getRandomTrackLyrics);
     gifGetter();
     clearArtistHint();
+    clearLyricsHint();
 })
 
 const artistHintButton = document.querySelector(".hint-artist");
@@ -23,6 +24,25 @@ const clearArtistHint = function() {
     console.log(artistHintButton.contains(artistName));
     if (artistHintButton.contains(artistName)) {
     artistHintButton.removeChild(artistName)
+    console.log("Artist hint cleared");
+}
+}
+
+const lyricsHintButton = document.querySelector(".hint-lyrics");
+let lyricsName = document.createElement('h3');
+
+lyricsHintButton.addEventListener('click', function() {
+    console.log(trackInfo.lyrics);
+    let lyricsHint = lyricsHintButton.appendChild(lyricsName);
+    lyricsHint.textContent = "Lyrics: " + trackInfo.lyrics;
+})
+
+const clearLyricsHint = function() {
+    console.log("inside clear artist function")
+    console.log("this is the artist name", lyricsName);
+    console.log(lyricsHintButton.contains(lyricsName));
+    if (lyricsHintButton.contains(lyricsName)) {
+        lyricsHintButton.removeChild(lyricsName)
     console.log("Artist hint cleared");
 }
 }
