342bf5cd4e27c6550ff756355551ec5c40c3d2a3
Reda <redahaq@gmail.com>
2019-11-14 18:36:01 +0000

    fix button glitch

--- src/scripts/script-run.js
+++ src/scripts/script-run.js
@@ -8,11 +8,13 @@ newGameButton.addEventListener('click', function() {
 while (gifContainer.firstChild) {
         gifContainer.removeChild(gifContainer.firstChild);
     }
+    clearArtistHint();
+    clearLyricsHint();
     getRandomTrackInfo(function() {
         getRandomTrackLyrics(gifGetter);
-    clearArtistHint();
-    });
 })
+})
+
 
 const artistHintButton = document.querySelector(".hint-artist");
 let artistName = document.createElement('h3');
@@ -32,3 +34,44 @@ const clearArtistHint = function() {
     console.log("Artist hint cleared");
 }
 }
+
+const lyricsHintButton = document.querySelector(".hint-lyrics");
+let lyricsName = document.createElement('h3');
+
+lyricsHintButton.addEventListener('click', function() {
+    console.log(trackInfo.lyrics);
+    let lyricsHint = lyricsHintButton.appendChild(lyricsName);
+    lyricsHint.textContent = "Lyrics: " + trackInfo.lyrics;
+});
+
+const clearLyricsHint = function() {
+    console.log("inside clear artist function")
+    console.log("this is the artist name", lyricsName);
+    console.log(lyricsHintButton.contains(lyricsName));
+    if (lyricsHintButton.contains(lyricsName)) {
+        lyricsHintButton.removeChild(lyricsName);
+    console.log("Artist hint cleared");
+}
+}
+
+// reveal-song
+
+const artistTrackHintButton = document.querySelector(".reveal-song");
+let artistTrackName = document.createElement('h3');
+
+artistTrackHintButton.addEventListener('click', function() {
+    console.log(trackInfo.artist);
+    let artistTrackHint = artistTrackHintButton.appendChild(artistTrackName);
+    artistTrackHint.textContent = "Song: " + trackInfo.artist + "- " + trackInfo.track;
+})
+
+const clearArtistTrackHint = function() {
+    console.log("inside clear artistTrack function")
+    console.log("this is the artist and track names", artistTrackName);
+    console.log(artistHintButton.contains(artistTrackName));
+    if (artistTrackHintButton.contains(artistTrackName)) {
+        artistTrackHintButton.removeChild(artistTrackName)
+    console.log("Artist and track hint cleared");
+}
+}
+
