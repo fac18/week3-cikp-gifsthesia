f177f1c32cc51ec88d82af2935a0a065a5c300ea
Jamie Carter <jc2820@hotmail.com>
2019-11-15 11:55:32 +0000

    Remove iife from giphy pull function Relates #54

--- src/scripts/script-gifs.js
+++ src/scripts/script-gifs.js
@@ -1,48 +1,41 @@
-
-
 function gifGetter() {
 
-let snippet = trackInfo.lyrics;
-let words = [];
+    let snippet = trackInfo.lyrics;
+    let words = [];
 
-const separateSnippet = (x) => {
-    let snippedSnippet = snippet.replace(/'/g, "");
-    words = snippedSnippet.split(" ");
-    console.log('this is before the loop', words);
-    return words;
-}
-
-separateSnippet(snippet);
-
-const gifStack = (w) => {
-    console.log('this is immediately before the loop', w);
-    for (let i=0; i<words.length; i++) {
-        let snippetWord = words[i];
-
-
-(function(){
-    let xhr = new XMLHttpRequest();
-
-    let url = "http://api.giphy.com/v1/gifs/search?q=" + snippetWord + "&api_key=dc6zaTOxFJmzC&limit=1";
-
-    console.log('this is snippetword', snippetWord);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            let giphyObj = JSON.parse(xhr.responseText);
-            console.log(giphyObj);
-            let newGif = document.createElement("img");
-            let gifContainer = document.querySelector(".gif-container");
-            gifContainer.appendChild(newGif);
-            let link = giphyObj.data[0].images.fixed_height.url;
-            newGif.src = link;
-            // wait(1000);
-        }
+    const separateSnippet = (x) => {
+        let snippedSnippet = snippet.replace(/'/g, "");
+        words = snippedSnippet.split(" ");
+        console.log('this is before the loop', words);
+        return words;
     }
-    xhr.open("GET", url, true)
-    xhr.send();
-})();
-};
-}
+
+    separateSnippet(snippet);
+
+    const gifStack = (w) => {
+        for (let i=0; i<words.length; i++) {
+            let snippetWord = words[i];
+            gifCall(snippetWord);
+
+            function gifCall() {
+                let xhr = new XMLHttpRequest();
+                let url = "http://api.giphy.com/v1/gifs/search?q=" + snippetWord + "&api_key=dc6zaTOxFJmzC&limit=1";
+        
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        let giphyObj = JSON.parse(xhr.responseText);
+                        let newGif = document.createElement("img");
+                        let gifContainer = document.querySelector(".gif-container");
+                        gifContainer.appendChild(newGif);
+                        let link = giphyObj.data[0].images.fixed_height.url;
+                        newGif.src = link;
+                    }
+                }
+            xhr.open("GET", url, true)
+            xhr.send();
+            };
+        };
+    }
 gifStack(words);
 };
 
